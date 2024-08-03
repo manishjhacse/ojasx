9a6a025ea50f301a7bcb377b0b208153570e7765
Siddhesh Kumar <siddheshkr21@gmail.com>
2024-08-03 18:09:32 +0530

    update

---
+++ client/src/components/Cards.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="max-w-[900px] px-8 mt-5">
+      <Card className="col-span-12 sm:col-span-4 h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">
+            What to watch
+          </p>
+          <h4 className="text-black font-medium text-large">
+            Stream the Acme event
+          </h4>
+        </CardHeader>
+        <Image
+          isZoomed
+          width={240}
+          alt="NextUI Fruit Image with Zoom"
+          src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
+        />
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">Available soon.</p>
+          <Button
+            className="text-tiny text-white bg-black/20"
+            variant="flat"
+            color="default"
+            radius="lg"
+            size="sm"
+          >
+            Notify me
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
