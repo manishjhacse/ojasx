0d91a3ff6a2c0f57e7cd3530b8899de6461c4a08
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-08-03 23:34:26 +0530

    structured dome folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,12 @@ import CreateEvent from "./pages/CreateEvent";
 import HomePage from "./pages/HomePage";
 export default function App() {
   return (
-    <>
+    <div className="prevent-select">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/createevent" element={<CreateEvent />} />
       </Routes>
       <Toaster position="top-center" reverseOrder={false} />
-    </>
+    </div>
   );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Events from '../components/Events'
-import Hero from './Hero/Hero'
+import Hero from '../components/Hero/Hero'
 export default function HomePage() {
   return (
     <div>
