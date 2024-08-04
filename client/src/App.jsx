95562271e9acca0e253994ffee22a38d8d538816
shyam kumar <sk5859915@gmail.com>
2024-08-04 12:18:57 +0530

    created signup form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CreateEvent from "./pages/CreateEvent";
 import HomePage from "./pages/HomePage";
+import SignUp from "./SignUpPage/SignUp";
 export default function App() {
   return (
     <div className="prevent-select">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/createevent" element={<CreateEvent />} />
+        <Route path="/signup" element={<SignUp />} />
+
       </Routes>
       <Toaster position="top-center" reverseOrder={false} />
     </div>
