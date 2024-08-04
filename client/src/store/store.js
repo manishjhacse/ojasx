f218b53bd9aa25e0dd85fd1c33c35b857bf211b1
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-08-05 01:40:04 +0530

    created card slice

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import eventSlice from "./eventSlice";
 import loggedInSlice from "./loginSlice";
 import userSlice from "./userSlice";
+import cartSlice from "./cartSlice";
 const store = configureStore({
   reducer: {
     events: eventSlice,
     loggedIn: loggedInSlice,
     loggedInUser: userSlice,
+    cart: cartSlice,
   },
 });
 export default store;
