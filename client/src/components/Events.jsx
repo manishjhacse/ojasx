27464ebcc6a26461ab7e5a2c1a30c5d477e124e2
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-08-04 15:02:47 +0530

    added card skelton

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -15,9 +15,21 @@ const Events = () => {
     <div className={`bg-black relative py-5 px-5  flex justify-center `}>
       <h1 className=" absolute top-2 z-50 font-bold text-5xl  text-white ">Events</h1>
       <div className=" w-[900px] flex mb-9 mt-16 flex-wrap gap-4  justify-center">
-        {eventsToShow.slice(0, 5)?.map((event) => (
+        {eventsToShow.length>0?
+        eventsToShow.slice(0, 5)?.map((event) => (
           <Cards key={event._id} event={event} />
-        ))}
+        )):[0,1,2,3,4].map((index)=>{
+          return  <div key={index} className="col-span-12 sm:col-span-4 h-[300px] w-[264px] bg-gray-200 rounded-lg animate-pulse">
+          <div className="h-3/4 bg-gray-300 rounded-t-lg"></div>
+          <div className="flex justify-between items-center p-2 bg-gray-200 rounded-b-lg">
+            <div className="flex flex-col">
+              <div className="h-4 w-1/2 bg-gray-300 rounded mb-2"></div>
+              <div className="h-4 w-1/4 bg-gray-300 rounded"></div>
+            </div>
+            <div className="h-8 w-20 bg-gray-300 rounded-lg"></div>
+          </div>
+        </div>
+        })}
       </div>
 
       <Button
