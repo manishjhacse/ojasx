272ef77afb6664d8519ae7b0f8508824526d7456
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-08-04 21:39:28 +0530

    fixed about and navbar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,11 +4,8 @@ import Hero from '../components/Hero/Hero'
 import  Category  from '../components/Category'
 import About from '../components/About/About'
 import Navbar from '../components/Navbar/Navbar'
-<<<<<<< Updated upstream
 import  Footer  from '../components/Footer'
-=======
 import FAQ from '../components/FAQ'
->>>>>>> Stashed changes
 export default function HomePage() {
   return (
     <div>
@@ -18,11 +15,8 @@ export default function HomePage() {
         <Events/>
         <Category/>
         <About/>
-<<<<<<< Updated upstream
-        <Footer/>
-=======
         <FAQ/>
->>>>>>> Stashed changes
+        <Footer/>
     </div>
   )
 }
